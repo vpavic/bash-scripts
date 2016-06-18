fb50176e04d11d402106f4b0344f9805616b4951
Vedran Pavic <vedran.pavic@gmail.com>
2016-06-19 00:17:23 +0200

    Update cntlm_noproxy_config script
    Add support for multiple corporate subnets

--- cntlm_noproxy_config.sh
+++ cntlm_noproxy_config.sh
@@ -8,7 +8,7 @@
 #
 # usage:
 # 1. make sure $CNTLM_CONF points to your CNTLM config file
-# 2. define $COMPANY_SUBNET to match IP address in your corporate network
+# 2. define $CORPORATE_SUBNET array to match IP address(es) in your corporate network
 # 3. customize $NO_PROXY_CORPORATE for your corporate network
 # 4. place this script in /etc/NetworkManager/dispatcher.d and configure order
 # 5. make script executable and set ownership to root:root
@@ -20,7 +20,7 @@
 # http://linux.die.net/man/8/networkmanager
 
 readonly CNTLM_CONF="/etc/cntlm.conf"
-readonly COMPANY_SUBNET="10.0.0"
+readonly CORPORATE_SUBNETS=("10.0.0")
 readonly NO_PROXY_CORPORATE="localhost, 127.0.0.*, 10.*, 192.168.*"
 readonly NO_PROXY_DIRECT="*"
 
@@ -31,17 +31,24 @@ fi
 
 case "$2" in
 	up|down|vpn-up|vpn-down)
-		hostname -I | grep -q $COMPANY_SUBNET
+		for subnet in "${CORPORATE_SUBNETS[@]}"; do
+			hostname -I | grep -q "$subnet"
+			match=$?
 
-		if [ $? -eq 0 ]; then
+			if [ $match -eq 0 ]; then
+				break
+			fi
+		done
+
+		if [ $match -eq 0 ]; then
 			logger "$(basename "$0") - set NoProxy for corporate net"
-			NO_PROXY=$NO_PROXY_CORPORATE
+			no_proxy=$NO_PROXY_CORPORATE
 		else
 			logger "$(basename "$0") - set NoProxy for direct net"
-			NO_PROXY=$NO_PROXY_DIRECT
+			no_proxy=$NO_PROXY_DIRECT
 		fi
 
-		sed "s/^NoProxy.*$/NoProxy\t\t$NO_PROXY/g" -i $CNTLM_CONF
+		sed "s/^NoProxy.*$/NoProxy\t\t$no_proxy/g" -i $CNTLM_CONF
 		service cntlm restart
 		;;
 esac
